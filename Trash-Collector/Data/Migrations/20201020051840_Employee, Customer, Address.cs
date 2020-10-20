bd5d78e603fb8c5e111ea0635c52f8a974d9579c
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-10-20 00:19:54 -0500

    Update DB to reflect changes in table information for Customer, Employee, Address

---
+++ Trash-Collector/Data/Migrations/20201020051840_Employee, Customer, Address.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trash_Collector.Data.Migrations
+{
+    public partial class EmployeeCustomerAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "784dd107-5843-4ee8-a4fd-78a862c91824");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "cd84639c-6404-4ea9-b54c-ab3d6fced999", "67f8c5ef-0d8c-412e-b339-2b03811d0d54", "Admin", "ADMIN" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cd84639c-6404-4ea9-b54c-ab3d6fced999");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "784dd107-5843-4ee8-a4fd-78a862c91824", "d057c2d9-1e48-4d3e-a694-ae8c65601f2e", "Admin", "ADMIN" });
+        }
+    }
+}
