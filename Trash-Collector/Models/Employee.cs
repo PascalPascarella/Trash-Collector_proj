868ba923f4475f0bb7aaee26222af1cec4dec44a
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-10-20 00:14:49 -0500

    Create Employee class and bind ID & Address foreign keys

--- Trash-Collector/Models/Employee.cs
+++ Trash-Collector/Models/Employee.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,21 @@ namespace Trash_Collector.Models
 {
 	public class Employee
 	{
+		[Key]
+		public int EmployeeId { get; set; }
+
+		[ForeignKey("IdentityUser")]
+		public int IdentityUserId { get; set; }
+		public IdentityUser	IdentityUser { get; set; }
+
+		[ForeignKey("Address")]
+		public string AddressId { get; set; }
+		public Address Address { get; set; }
+
+		[Display(Name = "First Name")]
+		public string FirstName { get; set; }
+
+		[Display(Name = "Last Name")]
+		public string LastName { get; set; }
 	}
 }
